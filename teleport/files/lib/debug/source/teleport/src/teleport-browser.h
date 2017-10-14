7cf89cebfd3acfbfef4e534da4dbe0a4d8f337f1
Julian Sparber <julian@sparber.net>
2017-10-14 21:19:10 +0200

    [fix] webpage build issue

--- teleport/files/lib/debug/source/teleport/src/teleport-browser.h
+++
@@ -1,7 +0,0 @@
-#ifndef __TELEPORT_BROWSER_H
-#define __TELEPORT_BROWSER_H
-
-int teleport_browser_run_avahi_service(TeleportPeer *);
-void teleport_browser_avahi_shutdown(void);
-
-#endif /* __TELEPORT_BROWSER_H */
--- teleport/files/lib/debug/source/teleport/src/teleport-publish.h
+++
@@ -1,8 +0,0 @@
-#ifndef __TELEPORT_PUBLISH_H
-#define __TELEPORT_PUBLISH_H
-
-int  teleport_publish_run               (gchar *);
-void teleport_publish_shutdown          (void);
-void teleport_publish_update            (gchar *);
-
-#endif /* __TELEPORT_PUBLISH_H */
