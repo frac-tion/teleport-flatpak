e230a4df05e8523fb69369ee57cb296bb8d91649
Julian Sparber <julian@sparber.net>
2017-10-14 21:08:09 +0200

    remove all build files

--- teleport/files/lib/debug/source/teleport/src/teleport-server.h
+++
@@ -1,9 +0,0 @@
-#ifndef __TELEPORT_SERVER_H
-#define __TELEPORT_SERVER_H
-
-int teleport_server_run         (void);
-int teleport_server_add_route   (gchar *,
-                                 gchar *,
-                                 gchar *);
-
-#endif /* __TELEPORT_SERVER_H */
